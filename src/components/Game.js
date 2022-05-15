aad3a2abdc88ddf1a7b2dffbb86080dcf47340a2
slickLink <lincoln.dube@outlook.com>
2022-05-15 11:03:52 -0400

    creates game squares given game type

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,10 +1,26 @@
-
+import GameSquare from "./GameSquare";
 
 const Game = ({game_type}) => {
     
+    // create Game Squares
+    const num_of_squares = game_type * game_type;
+    let game_squares = []
+    //build the array of game squares
+    for (let i = 0; i < num_of_squares; i++) {
+        game_squares.push(<GameSquare key={i.toString()}/>)
+    }
+
+    //calculate game position
+    let game_width = 9 * (game_type / 3);
+    let game_margin = 9 / (game_type / 3) - 3;
     return (
-        <div className="game">
-            Game
+        <div className="game"
+            style={{
+                width: `${game_width}rem`,
+                margin: `0rem ${game_margin}rem`}}>
+            
+            {game_squares}
+            {/* <Controls /> */}
         </div>
     )
 }
---
+++ src/components/GameSquare.js
@@ -0,0 +1,9 @@
+
+const GameSquare = () => {
+
+    return (
+        <p className="gameSquare">8</p>
+    )
+}
+
+export default GameSquare;
