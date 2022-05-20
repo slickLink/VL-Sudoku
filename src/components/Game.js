a207347aa0bcf486906d6cf413eb4f998db0a0c8
slickLink <lincoln.dube@outlook.com>
2022-05-20 12:03:23 -0400

    show latin square numbers on gameboard

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,27 +1,32 @@
 import GameSquare from "./GameSquare";
 import { useGame } from "../context/game";
-
+import { generateGame } from "../gameUtil";
 /*
   Game: is a resizable container that holds the game board
   resizing is dependent of the type of sudoku game (3,4,5,6,7,8,9).
   Game will generate the board (GameSquares) depending on the game type
 */
 const Game = () => {
+
     const [game] = useGame();
 
     // create Game Squares
-    const num_of_squares = game.type * game.type;
+    // const num_of_squares = game.type * game.type;
     const activeSquare = game.active;
+
+    // turn 2D array table to 1D array
+    let game_squares = generateGame(game.type).flat();
     //build the array of game squares
-    let game_squares = []
-    for (let i = 0; i < num_of_squares; i++) {
-        game_squares.push(<GameSquare 
-                                key={i.toString()} 
-                                game_type={game.type}
-                                active={ i === activeSquare ? 'active': undefined}
-                                subactive={ game.subactive.includes(i) ? 'subactive': undefined}
-                                position={i}/>)
-    }
+
+    game_squares = game_squares.map((value, i) => (
+        <GameSquare 
+            key={i.toString()} 
+            game_type={game.type}
+            active={ i === activeSquare ? 'active': undefined}
+            subactive={ game.subactive.includes(i) ? 'subactive': undefined}
+            position={i}
+            value={value}/>
+    ));
 
     return (
         <div className="game"
--- src/components/GameSquare.js
+++ src/components/GameSquare.js
@@ -5,7 +5,7 @@ import { useGame } from "../context/game";
   each GameSquare has the ability to be set active (blue), subactive(grey) or non-active (white).
   These squares are either mutable or immuatable once rendered
 */
-const GameSquare = ({game_type, active, subactive, position}) => {
+const GameSquare = ({game_type, active, subactive, position, value}) => {
     // retrieve game state
     const [, dispatchGame] = useGame();
 
@@ -21,7 +21,7 @@ const GameSquare = ({game_type, active, subactive, position}) => {
         <button 
             className={`gameSquare ${active} ${subactive}`}
             style={{width: `calc(100% / ${game_type})`}}
-            onClick={setActive}>8</button>
+            onClick={setActive}>{value}</button>
     )
 }
 
