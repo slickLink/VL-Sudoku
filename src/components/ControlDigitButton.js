8471dd1fc13bc7be30b6af88952a239826fa5e6c
slickLink <lincoln.dube@outlook.com>
2022-05-23 21:25:12 -0400

    setting a value to gamesquare from controlbutton

--- src/components/ControlDigitButton.js
+++ src/components/ControlDigitButton.js
@@ -1,11 +1,21 @@
-
+import { useGame } from "../context/game"
 /*
   Controls: renders a button control for user given a value
 */
 const ControlDigitButton = ({num}) => {
+    const [, dispatchGame] = useGame();
+
+    const registerPossibleValue = () => {
+        dispatchGame({
+            type: 'SET_ACTIVE_VALUE',
+            value: num
+        });
+    }
 
     return (
-        <button className="controldigitButton">{num}</button>
+        <button 
+        className="controldigitButton"
+        onClick={registerPossibleValue}>{num}</button>
     )
 }
 
