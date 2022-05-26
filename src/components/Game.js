f2317a986072e8bc5579259dfb91b88af9c11f98
slickLink <lincoln.dube@outlook.com>
2022-05-26 11:11:58 -0400

    sets active Gamesquare on and off

--- src/components/Game.js
+++ src/components/Game.js
@@ -14,89 +14,47 @@ const Game = ({game_board}) => {
     //get gamesquareobjects
     const getGameSquareObjectsfromValues = useCallback((activeIndex, game_board, subactive_arr, immuatables_arr) => {
         // converts game values to game objects
-        let game_state = game_board.map((value, i) => {
-            const is_active = i === activeIndex ? 'active': null;
-            const is_subactive = subactive_arr.includes(i) ? 'subactive': undefined;
-            const immutable = immuatables_arr.includes(i) ? true : false;
+        let game_state = game_board.map((value, index) => {
+            const is_active = index === activeIndex ? 'active': null;
+            const is_subactive = subactive_arr.includes(index) ? 'subactive': null;
+            const immutable = immuatables_arr.includes(index) ? true : false;
             const display_value = immutable ? value : null;
-            return ({
-                active: is_active,
-                subactive: is_subactive,
-                value: display_value
-            });
+            return (
+                <GameSquare
+                    key={index.toString()}
+                    active={is_active}
+                    subactive={is_subactive}
+                    position={index}
+                    value={display_value} />);
         });
         return game_state;
     },[]);
     
     //set internal state
     const [g_state, setGState] = useState(getGameSquareObjectsfromValues(game.active, game_board, game.subactive, game.immutable_squares));
+    const [active, setActive] = useState(game.active);
 
     //listen for game_board changes
     useEffect(() => {
-        let update_board = false;
         //handle change in game type
         if (g_state.length !== game_board.length){
-            update_board = true;
+            setGState(getGameSquareObjectsfromValues(active, game_board, game.subactive, game.immutable_squares));
         }
+    }, [g_state, game_board, getGameSquareObjectsfromValues, active, game.subactive, game.immutable_squares]);
 
-        if (g_state[game.active].active === 'active' && g_state.indexOf(game.active) !== game.active) {
-            update_board = true;
+    //listen for active GameSquare
+    useEffect(() => {
+        //handle change in game active index
+        if (game.active !== active) {
+            setActive(game.active)
+            setGState(getGameSquareObjectsfromValues(game.active, game_board, game.subactive, game.immutable_squares));
         }
-
-        //handle change in game.active
-        // if (g_state[game.active] !== game.active) {
-        //     const gameObject = g_state;
-        //     gameObject[game.active].active = 'active';
-        //     setGState(gameObject)
-        //     // console.log(g_state[game.active]);
-        // }
-        if (update_board) {
-            const gameObject = getGameSquareObjectsfromValues(game.active, game_board, game.subactive, game.immutable_squares)
-            setGState(gameObject);
-        }
-    }, [g_state, game_board, getGameSquareObjectsfromValues, game.active, game.subactive, game.immutable_squares]);
-
-
-    // create Game Squares
-    // const num_of_squares = game.type * game.type;
-   
-    // retrieve gamesquare values
-    //build the array of game squares
-    // let game_square_components = game_board.map((value, i) => {
-    //     const key = i.toString();
-    //     const is_active = i === activeSquare ? 'active': undefined;
-    //     const is_subactive = game.subactive.includes(i) ? 'subactive': undefined;
-    //     const immutable = game.immutable_squares.includes(i) ? true : false;
-    //     const display_value = immutable ? value : ' ';
-
-    //     return (
-    //         <GameSquare 
-    //             key={key} 
-    //             active={is_active}
-    //             subactive={is_subactive}
-    //             position={i}
-    //             value={display_value} /> )  
-    // });
-    
-    // set internal state
-
-    //listen for active value changes
-    // useEffect(() => {
-    //     if(game.active_value)
-    // })
-    const game_square_components = g_state.map((square, index) => (
-        <GameSquare
-            key={index.toString()}
-            active={square.active}
-            subactive={square.subactive}
-            position={index}
-            value={square.value} />
-    ))
+    }, [active, game.active, getGameSquareObjectsfromValues, game_board, game.subactive, game.immutable_squares]);
     // render
     return (
         <div className="game"
             style={{ width: `${game.width}rem` }}>
-            {game_square_components}
+            {g_state}
         </div>
     );
 }
